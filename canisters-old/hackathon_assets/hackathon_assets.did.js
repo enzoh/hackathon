2c386fade9b0b1bea723bc30f2e131cae77a66e9
Stanley Jones <stanley.jones@dfinity.org>
2020-06-26 10:47:56 -0700

    Fix: Delete folders again

--- canisters-old/hackathon_assets/hackathon_assets.did.js
+++
@@ -1,6 +0,0 @@
-export default ({ IDL }) => {
- const Path = IDL.Text
- const Contents = IDL.Text
- return IDL.Service({'retrieve': IDL.Func([Path], [Contents], ['query']),
-  'store': IDL.Func([Path, Contents], [], [])});
-};
--- canisters-old/hackathon_assets/hackathon_assets.js
+++
@@ -1,5 +0,0 @@
-import actorInterface from "ic:idl/hackathon_assets";
-
-export default icHttpAgent.makeActorFactory(actorInterface)({
-  canisterId: "ic:02000000000000000000000000000000000153",
-});
